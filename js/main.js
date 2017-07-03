30e8d3e4f30985bb558e0ae41072f22e13418ec8
jeffreynerona <jeffnerona@gmail.com>
2017-07-03 14:02:41 +0800

    buttons now working
    animated sections

--- js/main.js
+++ js/main.js
@@ -17,3 +17,20 @@ cheet('↑ ↑ ↓ ↓ ← → ← → b a', function () {
       $('#name').removeClass('enlarge');
   }, 1000);
 });
+
+$('#profile').click(function(){ expand('profile-section'); return false; });
+$('#portfolio').click(function(){ expand('portfolio-section'); return false; });
+$('#contact').click(function(){ expand('contact-section'); return false; });
+var active = 'a';
+
+function expand(x) {
+  if(active != x) {
+    $("#"+active).animate({
+              height: 0,
+          }, 500).removeClass('expand');
+    active = x;
+    $('#'+x).addClass('expand').animate({
+              height: 500,
+          }, 500);
+  }
+}
