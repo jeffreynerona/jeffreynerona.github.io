f15ea2c3a12f6186be7fa103937a2877d3f75ba5
jeffreynerona <jeffnerona@gmail.com>
2017-08-14 15:47:52 +0800

    redesigned content

--- js/main.js
+++ js/main.js
@@ -32,35 +32,50 @@ $('#contact').click(function(){
   expand('contact-section');
   return false;
 });
-var active = 'none';
+$('#close').click(function(){
+  close();
+  return false;
+});
 
 function expand(x) {
   var buttonLink = x.slice(0, x.length-8);
-  var ht = window.innerHeight - 172;
-  if(active=='none') {
-    $('#'+buttonLink).addClass('bluize');
-    $('#'+x).addClass('expand').animate({
-              height: ht,
-          }, 500);
-    active = x;
-  }
-  else if(active != x) {
-    var toRemove = active.slice(0, active.length-8);
-    $('#'+buttonLink).addClass('bluize');
-    $('#'+toRemove).removeClass('bluize');
-    $("#"+active).animate({
-              height: 0,
-          }, 500);
-    setTimeout(function(){
-      $("#"+active).removeClass('expand');
+  var ht = window.innerHeight;
+  $('#name').html(buttonLink.toUpperCase());
+  $('#titles').html('Jeffrey Nerona')
+  setTimeout(function(){
       $('#'+x).addClass('expand').animate({
-              height: ht,
-          }, 500);
-    active = x;
-    },500);
+            height: ht
+      }, 500);
+      $('header').addClass('fixed').animate({
+        top: "0px"
+      },500);
+      $('nav').addClass('remove');
+    },200);
+  
 
-  }
-  else {
+  $('.content').addClass('offset');
+  setTimeout(function(){
+      $('#close').addClass('expand');
+    },700);
+}
+
+function close() {
+  $('#close').removeClass('expand');
+  $('#name').html("Jeffrey Nerona");
+  $('#titles').html("Web Developer | Physics Geek | Cyborg");
+  $('.content').removeClass('offset').animate({
+            height: 0
+  }, 500);
+  setTimeout(function(){
+      $('.content').removeClass('expand');
+    },500);
+  $('nav').removeClass('remove');
+  $('header').removeClass('fixed');
+  $('header').css({
+    top: 'auto'
+  },500);
+}
+/*    
     $('#contact').removeClass('bluize');
     $('#profile').removeClass('bluize');
     $('#portfolio').removeClass('bluize');
@@ -71,5 +86,7 @@ function expand(x) {
       $("#"+x).removeClass('expand');
     },500);
     active='none';
-  }
-}
+    $('header').removeClass('fixed');
+
+    $('.content').removeClass('offset');
+*/
