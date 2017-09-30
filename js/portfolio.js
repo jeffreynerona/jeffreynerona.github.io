04d6f424b67f6bb98818dfff58fcaee8d0624b84
jeffreynerona <jeffnerona@gmail.com>
2017-09-30 23:49:26 +0800

    Added image sample in portfolio

--- js/portfolio.js
+++ js/portfolio.js
@@ -3,21 +3,21 @@
 	  "tags" : ["JavaScript", "Ruby", "MongoDB"],
 	  "title" : "Node Bank",
 	  "description" : "asdf",
-	  "image" : "images/1.png",
+	  "image" : "images/portfolio/sample.png",
 	  "github" : "https://github.com"
 	},
 	{ "id" : "2" , 
 	  "tags" : ["Php", "MySQL", "JavaSscript"],
 	  "title" : "Point of Sale",
 	  "description" : "asdfadsafas",
-	  "image" : "images/2.png",
+	  "image" : "images/portfolio/sample.png",
 	  "github" : "https://github.com"
 	},
 	{ "id" : "3" , 
 	  "tags" : ["Php", "MySQL", "JavaScript"],
 	  "title" : "React App",
 	  "description" : "asdfadsafas",
-	  "image" : "images/3.png",
+	  "image" : "images/portfolio/sample.png",
 	  "github" : "https://github.com"
 	}
 	];
@@ -28,7 +28,8 @@ function showItems() {
      var id = portfolio[i].id;
      var title = portfolio[i].title;
      var tags = portfolio[i].tags;
-     output += '<div class="portfolio-item" id="item-'+id+
+     var image =portfolio[i].image
+     output += '<div class="portfolio-item" style="background-image:url('+image+')" id="item-'+id+
      '"><div class="portfolio-details"><span class="portfolio-title">'+title
      +'</span><span class="portfolio-tags">'+tags[0]+' | '+tags[1]+' | '+tags[2]+'</span></div></div>';
   }
